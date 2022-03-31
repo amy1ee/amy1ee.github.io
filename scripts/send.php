4f5716bb7fe91f3c96924657e8d3160c0603b08c
Amy Lee <96388177+amy1ee@users.noreply.github.com>
2022-03-30 20:23:48 -0700

    Delete send.php

--- scripts/send.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $to = "amy.lee2514@gmail.com";
-    $subject = getElementById('subject');
-    $message = getElementById('message');
-    $headers = "From: ", getElementById('email');
-    mail($to, $subject, $message, $headers);
-?>;
